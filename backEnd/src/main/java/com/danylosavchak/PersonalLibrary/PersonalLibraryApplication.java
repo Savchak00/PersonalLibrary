184ab70f4f7fa0708898572ba0e6e1223b4aac43
Danylo Savchak <daniel.savchak@gmail.com>
2023-10-01 19:03:28 +0200

    fixed CORS problem for accesing the data from frontEnd

--- backEnd/src/main/java/com/danylosavchak/PersonalLibrary/PersonalLibraryApplication.java
+++ backEnd/src/main/java/com/danylosavchak/PersonalLibrary/PersonalLibraryApplication.java
@@ -2,12 +2,23 @@ package com.danylosavchak.PersonalLibrary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class PersonalLibraryApplication {
+public class PersonalLibraryApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PersonalLibraryApplication.class, args);
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/api/**")
+				.allowedOrigins("http://localhost:3000")
+				.allowedMethods("GET", "POST", "PUT", "DELETE")
+				.allowedHeaders("*")
+				.allowCredentials(true);
 	}
 
 }
