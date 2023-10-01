07c51a5bdf7cf8a9bd88134a8c9332706608a479
Danylo Savchak <daniel.savchak@gmail.com>
2023-10-01 19:09:39 +0200

    Created logIn page and connected to backEnd

---
+++ frontEnd/personallibrary/src/LogIn/LogIn.js
@@ -0,0 +1,50 @@
+import styles from './LogIn.module.css';
+import {useState} from 'react';
+import logo from '../logo.svg'
+import Axios from "axios";
+
+export const LogIn = (props) => {
+    const [name,setName] = useState("");
+    const [surName,setSurName] = useState("");
+    const [email,setEmail] = useState("");
+    
+    const handleChangeName = (event) => {
+        setName(event.target.value);
+    };
+
+    const handleChangeSurName = (event) => {
+        setSurName(event.target.value);
+    };
+
+    const handleChangeEmail = (event) => {
+        setEmail(event.target.value);
+    };
+
+    const buttonClicked = () => {
+        if (name == "" || surName == "" || email == "") {
+            alert("Fields cannot be empty");
+        } else {
+            Axios.post("http://172.20.10.8:8080/api/logIn/logInOrCreate", {
+                "firstName" : name,
+                "lastName" : surName,
+                "email" : email
+            })
+            .then((response) => {
+                sessionStorage.setItem("userId", response.data["responseData"]["userId"]);
+                // console.log(sessionStorage.getItem("userId"));
+            });
+        }
+    };
+
+    return (
+        <div className={styles.LogIn}>
+            <img src={logo} alt="Logo" />
+            <div className={styles.InputDiv}>
+                <input className={styles.Input} onChange={handleChangeName} placeholder='Name' style={{backgroundColor:"#DAC9F5"}}/>
+                <input className={styles.Input} onChange={handleChangeSurName} placeholder='Surname' style={{backgroundColor:"#FEF1CF"}}/>
+                <input className={styles.Input} onChange={handleChangeEmail} placeholder='Email' style={{backgroundColor:"#D7FFD7"}}/>
+            </div>
+            <button className={styles.Button} onClick={buttonClicked} >Log In</button>
+        </div>
+    );
+};
