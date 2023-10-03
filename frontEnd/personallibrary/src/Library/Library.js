d99003fd149e0d2c9bc0dd1f58d16b036e830770
Danylo Savchak <daniel.savchak@gmail.com>
2023-10-03 11:59:35 +0200

    Updated library screen

--- frontEnd/personallibrary/src/Library/Library.js
+++ frontEnd/personallibrary/src/Library/Library.js
@@ -3,16 +3,33 @@ import {LibraryBook} from './LibraryBook/LibraryBook';
 import logo from '../logo.svg';
 import Flickity from 'react-flickity-component';
 import './flikity.css'
+import {useEffect, useState} from 'react';
+import Axios from "axios";
 
 export const Library = (props) => {
+    const [library, setLibrary] = useState([]);
+
+    useEffect(() => {
+      fetchLibrary();
+    },[]);
+
+    const fetchLibrary = () => {
+      Axios.get(`http://172.20.10.8:8080/api/library/${sessionStorage.getItem("userId")}`, {
+      })
+      .then((response) => {
+        const lib = response.data["responseData"]["library"];
+        setLibrary([...library, ...lib]);
+      });
+      console.log(library);
+  };
 
     const buttonClicked = () => {
     
     };
   
     const flickityOptions = {
-      initialIndex: 2,
-      groupCells: 2
+      initialIndex: library.length/2,
+      groupCells: library.length/5
     }
 
     return (
@@ -20,32 +37,14 @@ export const Library = (props) => {
         <img src={logo} className={styles.LogoLibrary} alt="Logo" />
             <div className={styles.ListOfBooks}>
               <Flickity className={'carousel'} elementType={'div'} options={flickityOptions} disableImagesLoaded={false} reloadOnUpdate static >
-                <LibraryBook text="Clean Code" color="#A97AF6" angle={-5}/>
-                <LibraryBook text="Clean Architecture" color="#7ABBF6" angle={5}/>
-                <LibraryBook text="Essays" color="#F67A7A" angle={0}/>
-                <LibraryBook text="Essays" color="#F67A7A" angle={0}/>
-                <LibraryBook text="Essays" color="#F67A7A" angle={0}/>
-                <LibraryBook text="Essays" color="#F67A7A" angle={0}/>
-                <LibraryBook text="Essays" color="#F67A7A" angle={0}/>
-                <LibraryBook text="Essays" color="#F67A7A" angle={0}/>
-                <LibraryBook text="Essays" color="#F67A7A" angle={0}/>
-                <LibraryBook text="Essays" color="#F67A7A" angle={0}/>
-                <LibraryBook text="Essays" color="#F67A7A" angle={0}/>
-                <LibraryBook text="Essays" color="#F67A7A" angle={0}/>
-                <LibraryBook text="Essays" color="#F67A7A" angle={0}/>
-                <LibraryBook text="Essays" color="#F67A7A" angle={0}/>
-                <LibraryBook text="Essays" color="#F67A7A" angle={0}/>
-                <LibraryBook text="Essays" color="#F67A7A" angle={0}/>
-                <LibraryBook text="Essays" color="#F67A7A" angle={0}/>
-                <LibraryBook text="Essays" color="#F67A7A" angle={0}/>
-                <LibraryBook text="Essays" color="#F67A7A" angle={0}/>
-                <LibraryBook text="Essays" color="#F67A7A" angle={0}/>
-                <LibraryBook text="Essays" color="#F67A7A" angle={0}/>
-
+              {library.map((book, index) => (
+                <LibraryBook
+                  key={index}
+                  book={book}
+                />
+              ))}
               </Flickity>
-            </div>
-            <div>
-
+              
             </div>
             <button className={styles.Button} onClick={buttonClicked} >Add</button>
         </div>
--- frontEnd/personallibrary/src/Library/LibraryBook/LibraryBook.js
+++ frontEnd/personallibrary/src/Library/LibraryBook/LibraryBook.js
@@ -1,10 +1,29 @@
 import styles from './LibraryBook.module.css';
+import { useNavigate } from "react-router-dom";
 
 export const LibraryBook = (props) => {
+    const book = props.book;
+    let navigate = useNavigate(); 
+    const angles = [-5, -3, 3, 5];
+
+    const getAngle = () => {
+        return angles[Math.floor(Math.random() * angles.length)];
+    }
+
+    const backgroundColors = ["#A97AF6", "#7ABBF6","#F67A7A", "#847AF6", "#F6B67A", "#21A61E"];
+
+    const getBackColor = () => {
+        return backgroundColors[Math.floor(Math.random() * backgroundColors.length)];
+    };
+
+    const buttonClicked = () => {
+        navigate('/book');
+    };
+
     return (
         <div className={styles.cont}>
-            <div className={styles.Book} style={{backgroundColor: props.color, transform: "rotate(" + props.angle + "deg)"}}>
-                <p className={styles.Text}>{props.text}</p>
+            <div className={styles.Book}   role='button' onClick = {buttonClicked} style={{backgroundColor: getBackColor(), transform: "rotate(" + getAngle() + "deg)"}}>
+                <p className={styles.Text}>{book.title}</p>
             </div>
         </div>
     );
